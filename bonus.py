f585fda6869f199f0162e7b1d6e44f8e7b82ad0a
THEO POSENEL <theo.posenel@gmail.com>
2023-04-11 14:01:27 +0200

    projet python

--- bonus.py
+++ bonus.py
@@ -29,12 +29,22 @@ t8 = Task()
 t8.name = "T8"
 
 # * La liste de dépendance entre les tâches
-dependencies = {"T1": [], "T2": ["T1"], "T3": ["T1"], "T4":["T1"], "T5":["T2", "T3"], "T6":["T4", "T2"], "T7":["T3", "T4"], "T8":["T5", "T6", "T7"]}
+dependenciesGraphe2 = {"T1": [], "T2": ["T1"], "T3": ["T1"], "T4":["T1"], "T5":["T2", "T3"], "T6":["T4", "T2"], "T7":["T3", "T4"], "T8":["T5", "T6", "T7"]}
 
-s2 = TaskSystem([t1, t2, t3, t4, t5, t6, t7, t8], dependencies)
+dependenciesGraphe4 = {"T1": [], "T2": ["T1"], "T3": ["T1"], "T4":["T1"], "T5":["T2", "T3"], "T6":["T3", "T4"], "T7":["T4"], "T8":["T5", "T6", "T7"]}
+
+s2 = TaskSystem([t1, t2, t3, t4, t5, t6, t7, t8], dependenciesGraphe2)
+
+s3 = TaskSystem([t1, t2, t3, t4, t5, t6, t7, t8], dependenciesGraphe4)
 
 print(s2.tasks)
 
-s2.verifier_entrees([t1, t2, t3, t4, t5, t6, t7, t8], dependencies)
+print(s3.tasks)
+
+s2.verifier_entrees([t1, t2, t3, t4, t5, t6, t7, t8], dependenciesGraphe2)
+
+s3.verifier_entrees([t1, t2, t3, t4, t5, t6, t7, t8], dependenciesGraphe4)
 
 s2.runSeq()
+
+s3.runSeq()
